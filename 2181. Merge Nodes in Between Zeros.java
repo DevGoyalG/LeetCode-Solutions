1ba33216915f8c9afe2f5b6b28e03c94d7203dc4
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-07-20 00:49:53 +0530

    LeetCode Solutions

---
+++ 2181. Merge Nodes in Between Zeros.java
@@ -0,0 +1,18 @@
+class Solution {
+    public ListNode mergeNodes(ListNode head) {
+        head = head.next;
+        ListNode start = head;
+        while (start != null) {
+            ListNode end = start;   
+            int sum = 0;
+            while (end.val != 0) {
+                sum += end.val;
+                end = end.next;
+            }
+            start.val = sum;   
+            start.next = end.next;   
+            start = start.next;
+        }
+        return head;
+    }
+}
