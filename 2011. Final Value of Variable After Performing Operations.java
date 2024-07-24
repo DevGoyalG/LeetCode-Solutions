225cc5113ea5d18adeddbf6a76228a75ee022d7a
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-07-25 00:35:12 +0530

    LeetCode Solutions

---
+++ 2011. Final Value of Variable After Performing Operations.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int finalValueAfterOperations(String[] operations) {
+        int X=0;
+        for(int i=0; i<operations.length; i++){
+            if(operations[i].equals("++X") || operations[i].equals("X++")){
+                X=X+1;
+            }
+                
+            else{
+                X=X-1;
+            }
+        }
+        return X;
+    }
+}
