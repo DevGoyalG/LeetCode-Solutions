11e95134d502625e920716da59967f89f6774f50
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2024-07-26 00:32:18 +0530

    LeetCode Solutions

---
+++ 1662. Check If Two String Arrays are Equivalent.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String str1="", str2="";
+        for(String s:word1){
+            str1+=s;
+        }
+        for(String s:word2){
+            str2+=s;
+        }
+        return str1.equals(str2);
+    }
+}
