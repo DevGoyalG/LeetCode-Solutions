7fc2853df379aaf0aa4e130c0bae88ff409f1c63
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-01-06 22:32:04 +0530

    LeetCode Solutions

---
+++ 1221. Split a String in Balanced Strings.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int balancedStringSplit(String s) {
+        int sum=0,count=0;
+        for(char c:s.toCharArray()){
+            if(c=='R')
+            count++;
+            else
+            count--;
+            if(count==0)
+            sum+=1;
+        }
+        return sum;
+    }
+}
