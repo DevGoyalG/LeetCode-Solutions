d0cd203bb0874c86c28f3e2a99df52dd84c538df
Dev Goyal <127044988+DevGoyalG@users.noreply.github.com>
2025-01-14 21:17:12 +0530

    LeetCode Solutions

---
+++ 921. Minimum Add to Make Parentheses Valid.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def minAddToMakeValid(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        open_brackets = 0
+        mismatch = 0
+        for char in s:
+            if char == '(':
+                open_brackets += 1
+            else:
+                if open_brackets > 0:
+                    open_brackets -= 1
+                else:
+                    mismatch += 1
+        return open_brackets + mismatch
